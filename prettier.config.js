d4f36bc654148892960e9ffc638b4cdc4f8526f2
kduarte <katiaipduarte@hotmail.com>
2023-09-22 11:45:20 +0100

    add vscode settings, eslint and prettier rules

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	plugins: ['prettier-plugin-tailwindcss'],
+	endOfLine: 'lf',
+	printWidth: 80,
+	trailingComma: 'es5',
+	semi: false,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	useTabs: true,
+	tabWidth: 2,
+	arrowParens: 'always',
+	htmlWhitespaceSensitivity: 'strict',
+}
