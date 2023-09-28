04beeee829e8d89b13d4bee014f864655af19a5a
Katia Duarte <katiaipduarte@hotmail.com>
2023-09-28 12:22:21 +0100

    build: update tailwind rules. add bundle analyzer plugin

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+	openAnalyzer: false,
+})
 
-module.exports = nextConfig
+module.exports = withBundleAnalyzer({
+	reactStrictMode: true,
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,59 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+	darkMode: 'class',
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./index.html',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./app/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				body: [
+					'Inter',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'system-ui',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+				sans: [
+					'Inter',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'system-ui',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+			},
+			borderWidth: {
+				DEFAULT: '1px',
+				0: '0',
+				2: '2px',
+				3: '3px',
+				4: '4px',
+				6: '6px',
+				8: '8px',
+			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic':
@@ -16,5 +62,8 @@ const config: Config = {
 		},
 	},
 	plugins: [],
+	futureuture: {
+		hoverOnlyWhenSupported: true,
+	},
 }
 export default config
